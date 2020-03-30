82247e9ac30e9d2657d0c55ca8fcb5f7a704ee68
Fabian Franco <fabito.10@hotmail.com>
2020-03-29 19:41:18 -0500

    Version 3

--- js/mainDash.js
+++ js/mainDash.js
@@ -5,6 +5,7 @@ function initApp() {
 
     cargarPuntajesBank()
     cargarPuntajesSpace()
+    cargarPuntajesMario()
 }
 
 function cargarPuntajesBank() {
@@ -105,9 +106,103 @@ function cargarPuntajesSpace() {
     });
 }
 
+function cargarPuntajesSpace() {
+    ///User information is obtained
+    var infoPuntSpace = firebase.database().ref().child("monsterSpace")
+    var infoPuntSpacePromise = infoPuntSpace.on('value', function (snapshot) {
+
+        var puntajes= []
+        var obj = new Object();
+        var dataComplete = [];
+        var info = (snapshot.val())
+        //  var objStudent = new Object();
+        //  studentList = [];
+        var puntajeAlto = 0;
+        var nombrePuntajeAlto = "";
+
+        snapshot.forEach(function (child) {
+
+            
+            var nombre = child.key
+
+            obj.nombre = nombre;
+            
+
+            child.forEach(function (childMin) {
+
+                obj.fecha = unixToTimeStampOurTimeZone (parseInt(childMin.key));
+
+                var puntajeActual  = childMin.val();
+                obj.puntaje = puntajeActual;
+    
+                if(puntajeActual > puntajeAlto){
+                    puntajeAlto = puntajeActual;
+                    nombrePuntajeAlto = nombre;
+                }
+                
+    
+            });
+
+
+            puntajes.push(obj)
+            obj = {}
+        });
+        $("#mejorPuntajeSpace").empty();
+        $("#mejorPuntajeSpace").append(nombrePuntajeAlto + " " + puntajeAlto);
+        makeTableSpace(puntajes)
+
+        
+    });
+}
+
+function cargarPuntajesMario() {
+    ///User information is obtained
+    var infoPuntMario = firebase.database().ref().child("mario")
+    var infoPuntMarioPromise = infoPuntMario.on('value', function (snapshot) {
+
+        var puntajes= []
+        var obj = new Object();
+        var dataComplete = [];
+        var info = (snapshot.val())
+        //  var objStudent = new Object();
+        //  studentList = [];
+        var puntajeAlto = 0;
+        var nombrePuntajeAlto = "";
+
+        snapshot.forEach(function (child) {
+
+            
+            var nombre = child.key
+
+            obj.nombre = nombre;
+            
+
+            child.forEach(function (childMin) {
+
+                obj.fecha = unixToTimeStampOurTimeZone (parseInt(childMin.key));
+
+                var puntajeActual  = childMin.val();
+                obj.puntaje = puntajeActual;
+    
+                if(puntajeActual > puntajeAlto){
+                    puntajeAlto = puntajeActual;
+                    nombrePuntajeAlto = nombre;
+                }
+                
+    
+            });
+
+
+            puntajes.push(obj)
+            obj = {}
+        });
+        $("#mejorPuntajeMario").empty();
+        $("#mejorPuntajeMario").append(nombrePuntajeAlto + " " + puntajeAlto);
+        makeTableMario(puntajes)  
+    });
+}
+
 function makeTable(obj) {
-
-
 
     $("#tableBankHeroData").empty();
     var table = '';
@@ -125,8 +220,6 @@ function makeTable(obj) {
 
 function makeTableSpace(obj) {
 
-
-
     $("#tableSpaceData").empty();
     var table = '';
 
@@ -139,6 +232,22 @@ function makeTableSpace(obj) {
     }
 
     $("#tableSpaceData").append(table);
+}
+
+function makeTableMario(obj) {
+
+    $("#tableMarioData").empty();
+    var table = '';
+
+    for (let i = 0; i < obj.length; i++) {
+        table = table.concat('<tr>')
+        table = table.concat('<td>' + obj[i].nombre + '</td>')
+        table = table.concat('<td>' + obj[i].fecha + '</td>')
+        table = table.concat('<td>' + obj[i].puntaje + '</td>')
+        table = table.concat('</tr>')
+    }
+
+    $("#tableMarioData").append(table);
 }
 
 
